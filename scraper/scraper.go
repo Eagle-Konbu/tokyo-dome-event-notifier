828fbed6ee3668aa04d41e1b852f1a37430c3ad7
Atsuya Uchida <atsuya_eagle@outlook.com>
2022-04-04 21:05:35 +0900

    Added category to message.

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -23,6 +23,7 @@ func FetchTodayEvent() string {
 	selector := "div.c-mod-tab__body:nth-child(2) > table > tbody"
 	innerSelector := "tr.c-mod-calender__item"
 	dateSelector := "th > span:nth-child(1)"
+	categorySelector := "td:nth-child(2) > div > div:nth-child(1) > p > span"
 	titleSelector := "td > div > div:nth-child(2) > p.c-mod-calender__links"
 	timeSelector := "td > div > div:nth-child(2) > p:nth-child(2)"
 
@@ -31,14 +32,15 @@ func FetchTodayEvent() string {
 	var event string
 	selection.Find(innerSelector).Each(func(index int, s *goquery.Selection) {
 		date, _ := strconv.Atoi(s.Find(dateSelector).Text())
+		category := s.Find(categorySelector).Text()
 		title := s.Find(titleSelector).Text()
 		info := s.Find(timeSelector).Text()
 
-		if date == time.Now().Day() {
+		if date == time.Now().Day()-1 {
 			if title == "" {
 				event = "イベントなし"
 			} else {
-				event = title + "\n" + info
+				event = title + "（" + category + "）" + "\n" + info
 			}
 		}
 	})
