56c3304adc18a81c37f649a12eb8bf04140e308d
Atsuya Uchida <atsuya_eagle@outlook.com>
2022-04-04 09:52:29 +0900

    init.

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/tokyo-dome-event-notifyer
+
+go 1.17
+
+require github.com/aws/aws-lambda-go v1.6.0
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+)
---
+++ scraper/scraper.go
@@ -0,0 +1,46 @@
+package scraper
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func FetchTodayEvent() string {
+	res, err := http.Get("https://www.tokyo-dome.co.jp/dome/event/schedule.html")
+
+	if err != nil {
+		fmt.Println("Failed to scrape")
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	doc, _ := goquery.NewDocumentFromReader(res.Body)
+
+	selector := "div.c-mod-tab__body:nth-child(2) > table > tbody"
+	innerSelector := "tr.c-mod-calender__item"
+	dateSelector := "th > span:nth-child(1)"
+	titleSelector := "td > div > div:nth-child(2) > p.c-mod-calender__links"
+	timeSelector := "td > div > div:nth-child(2) > p:nth-child(2)"
+
+	selection := doc.Find(selector)
+
+	var event string
+	selection.Find(innerSelector).Each(func(index int, s *goquery.Selection) {
+		date, _ := strconv.Atoi(s.Find(dateSelector).Text())
+		title := s.Find(titleSelector).Text()
+		info := s.Find(timeSelector).Text()
+
+		if date == time.Now().Day() {
+			if title == "" {
+				event = "イベントなし"
+			} else {
+				event = title + "\n" + info
+			}
+		}
+	})
+	return event
+}
