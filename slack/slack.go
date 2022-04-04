c4700884252b12c229e9fe8fe66cc076a28435f3
Atsuya Uchida <atsuya_eagle@outlook.com>
2022-04-04 17:31:11 +0900

    updated to send message.

--- slack/slack.go
+++ slack/slack.go
@@ -1,48 +1,50 @@
 package slack
 
 import (
+	"bytes"
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func SendEventInfo(text string) {
 	url := os.Getenv("SLACK_WEBHOOK_URL")
-	// body := fmt.Sprintf(`{
-	// 	"blocks": [
-	// 		{
-	// 			"type": "header",
-	// 			"text": {
-	// 				"type": "plain_text",
-	// 				"text": "本日のイベント情報"
-	// 			}
-	// 		},
-	// 		{
-	// 			"type": "divider"
-	// 		},
-	// 		{
-	// 			"type": "section",
-	// 			"text": {
-	// 				"type": "plain_text",
-	// 				"text": "%s",
-	// 				"emoji": true
-	// 			}
-	// 		}
-	// 	]
-	// }`, text)
+	body := fmt.Sprintf(`{
+		"text": "本日のイベント情報",
+		"blocks": [
+			{
+				"type": "header",
+				"text": {
+					"type": "plain_text",
+					"text": "本日のイベント情報"
+				}
+			},
+			{
+				"type": "divider"
+			},
+			{
+				"type": "section",
+				"text": {
+					"type": "plain_text",
+					"text": "%s",
+					"emoji": true
+				}
+			}
+		]
+	}`, text)
 
-	// req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(body)))
-	// req.Header.Set("Content-Type", "application/json")
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(body)))
+	req.Header.Set("Content-Type", "application/json")
 
-	// if err != nil {
-	// 	panic(err)
-	// }
+	if err != nil {
+		panic(err)
+	}
 
-	// client := new(http.Client)
-	// res, err := client.Do(req)
+	client := new(http.Client)
+	res, err := client.Do(req)
 
-	// if err != nil {
-	// 	panic(err)
-	// }
-	fmt.Println(url)
-	// defer res.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
 }
